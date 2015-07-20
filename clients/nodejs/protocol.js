d857730aeb2cc636cf14c9c868b60ec21091d8b1
Muhammed Thanish <mnmtanish@gmail.com>
2015-07-20 15:30:16 +0530

    Move client packages to separate repos

--- clients/nodejs/protocol.js
+++
@@ -1,5 +0,0 @@
-var ProtoBuf = require('protobufjs');
-var path = require('path');
-var fpath = path.resolve(__dirname, '../../kmdb/protocol.proto');
-var builder = ProtoBuf.loadProtoFile(fpath);
-module.exports = builder.build('kmdb');
