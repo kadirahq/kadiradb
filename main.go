fc7ef36e60d42301891a804cb4ff07b4f33d8bc7
Muhammed Thanish <mnmtanish@gmail.com>
2015-07-27 12:55:13 +0530

    Auto create databases

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	_ "net/http/pprof"
 
+	"github.com/gogo/protobuf/proto"
 	"github.com/kadirahq/kadiradb-metrics/kmdb"
 )
 
@@ -22,6 +25,7 @@ const (
 func main() {
 	addr := flag.String("addr", DefaultAddr, "server address")
 	path := flag.String("path", "", "path to store data files")
+	init := flag.String("init", "", "path of initial dbs file")
 	pprof := flag.Bool("pprof", false, "enable pprof")
 	write := flag.Bool("write", false, "enable recovery")
 
@@ -41,6 +45,10 @@ func main() {
 		Recovery: *write,
 	})
 
+	if *init != "" {
+		createDatabases(s, *init)
+	}
+
 	if err != nil {
 		panic(err)
 	}
@@ -50,6 +58,46 @@ func main() {
 	}
 
 	log.Println(s.Listen())
+}
+
+func createDatabases(s kmdb.Server, name string) {
+	file, err := os.Open(name)
+	if err != nil {
+		panic(err)
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	sz := info.Size()
+	data := make([]byte, sz)
+
+	n, err := file.Read(data)
+	if err != nil {
+		panic(err)
+	} else if int64(n) != sz {
+		panic("error reading init file")
+	}
+
+	var reqs []*kmdb.OpenReq
+	err = json.Unmarshal(data, &reqs)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, req := range reqs {
+		reqData, err := proto.Marshal(req)
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = s.Open(reqData)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 func startPPROF() {
